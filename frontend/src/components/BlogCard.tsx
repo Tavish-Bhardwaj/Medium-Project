ff87a926e0e3e0f82b059e870950ac9ebaee7f14
tavish <bhardwajtavishofficial@gmail.com>
2024-07-30 01:14:57 +0530

    Appbar component created.

---
+++ frontend/src/components/BlogCard.tsx
@@ -0,0 +1,47 @@
+interface BlogCardProps {
+  authorName: string;
+  title: string;
+  content: string;
+  publishedDate: string;
+}
+
+export const BlogCard = ({
+  authorName,
+  title,
+  content,
+  publishedDate,
+}: BlogCardProps) => {
+  return (
+    <div className="p-4 border-b border-slate-200 pb-4 ">
+      <div className="flex">
+        <div className="flex justify-center flex-col">
+          <Avatar name={authorName} />
+        </div>
+        <div className="font-extralight pl-2">{authorName}</div>
+        <div className="flex justify-center flex-col pl-2" ><Circle /> </div>
+        <div className="pl-2 font-thin text-slate-400">{publishedDate}</div>
+      </div>
+      <div className="text-xl font-semibold">{title}</div>
+      <div className="text-md font-thin">{content.slice(0, 100) + "..."}</div>
+      <div className="text-slate-500 text-sm font-thin">{`${Math.ceil(content.length / 100)} minutes(s) read`}</div>
+      
+    </div>
+  );
+};
+
+export function Avatar({ name, size="small" }: { name: string, size?: "small" | "big" }) {
+  return (
+    <div className={`realtive inline-flex items-center justify-center ${size==="small"?"w-6 h-6":"w-10 h-10"} overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600`}>
+      <span className={` ${size=== "small"? "text-sx":"etxt-md"} font-extralight text-gray-600 dark:text-gray-300`}>
+        {name[0]}
+      </span>
+    </div>
+  );
+}
+
+
+function Circle(){
+    return <div className="h-1 w-1 rounded-full bg-slate-500 ">
+
+    </div>
+}
---
+++ frontend/src/pages/Blogs.tsx
@@ -0,0 +1,37 @@
+import { Appbar } from "../components/Appbar"
+import { BlogCard } from "../components/BlogCard"
+
+export const Blogs= ()=>{
+    return <div>
+
+    <Appbar/>
+     <div className="flex justify-center">
+    <div className=" max-w-xl">
+        <BlogCard
+        authorName={"Tavish"}
+        title={"How an ugly Single website makes $5000 a month without Affiliate marketing"}
+        content={"No need to create a fancy and modern  website with hundreds of pages to make money online. - Making money online is the dream for many people accross the globe.this is also some extra text i am just writing to check how it impacts the minutes read. currently it is showing the blog as a 2 minutes read but now depends how many minutes read it is for normal people according to the logic written by me here. "}
+        publishedDate={"28 July 2024"}
+        />
+        <BlogCard
+        authorName={"Tavish"}
+        title={"How an ugly Single website makes $5000 a month without Affiliate marketing"}
+        content={"No need to create a fancy and modern  website with hundreds of pages to make money online. - Making money online is the dream for many people accross the globe.this is also some extra text i am just writing to check how it impacts the minutes read. currently it is showing the blog as a 2 minutes read but now depends how many minutes read it is for normal people according to the logic written by me here. "}
+        publishedDate={"28 July 2024"}
+        />
+        <BlogCard
+        authorName={"Tavish"}
+        title={"How an ugly Single website makes $5000 a month without Affiliate marketing"}
+        content={"No need to create a fancy and modern  website with hundreds of pages to make money online. - Making money online is the dream for many people accross the globe.this is also some extra text i am just writing to check how it impacts the minutes read. currently it is showing the blog as a 2 minutes read but now depends how many minutes read it is for normal people according to the logic written by me here. "}
+        publishedDate={"28 July 2024"}
+        />
+        <BlogCard
+        authorName={"Tavish"}
+        title={"How an ugly Single website makes $5000 a month without Affiliate marketing"}
+        content={"No need to create a fancy and modern  website with hundreds of pages to make money online. - Making money online is the dream for many people accross the globe.this is also some extra text i am just writing to check how it impacts the minutes read. currently it is showing the blog as a 2 minutes read but now depends how many minutes read it is for normal people according to the logic written by me here. "}
+        publishedDate={"28 July 2024"}
+        />
+    </div>
+        </div>
+        </div>
+}
